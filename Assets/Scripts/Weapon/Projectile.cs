7b089dec4ea7e9419adfd6312b1e15e146089734
Zhenzhi <caizhenzhi5@gmail.com>
2025-06-28 00:37:08 +0800

    added projectile

---
+++ Assets/Scripts/Weapon/Projectile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile", menuName = "Scriptable Objects/Weapon")]
+public class Projectile : ScriptableObject
+{
+    public int damage = 1;
+    public int distance = 10;
+    public Vector2 movement = new Vector2();
+    public int speed = 10;
+
+    public void Initialize(int initDamge, int initDistance, Vector2 initMovement)
+    {
+        damage = initDamge;
+        distance = initDistance;
+        movement = initMovement;
+    }
+}
