25f5e8fddcbc73ac49e4ab7fbb882d9ef6aa99c8
Cai Zhenzhi <caizhenzhi@Cais-MacBook-Air-2.local>
2025-06-28 14:15:16 +0800

    Add API call

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -7,13 +7,15 @@ public class Weapon : ScriptableObject
     public int distance = 10;
     public int weight = 10;
     public float rate = 1.0f;
+    public GameObject projectileSpawned;
 
 
-    public void Initialize(int initDamge, int initDistance, int initWeight, float initRate)
+    public void Initialize(int initDamge, int initDistance, int initWeight, float initRate, GameObject gameObject)
     {
         damage = initDamge;
         distance = initDistance;
         weight = initWeight;
         rate = initRate;
+        projectileSpawned = gameObject;
     }
 }
