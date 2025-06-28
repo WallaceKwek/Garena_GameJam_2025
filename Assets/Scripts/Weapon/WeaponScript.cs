caeb5de21bd57ced9d8c7bc0093fa2b0b79820d7
Cai Zhenzhi <caizhenzhi@Cais-MacBook-Air-2.local>
2025-06-28 10:46:33 +0800

    Added weapon

--- Assets/Scripts/Weapon/WeaponScript.cs
+++ Assets/Scripts/Weapon/WeaponScript.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
-{
+{   
+    Projectile projectile;
+    public GameObject projectileSpawned;
 
+    public void Attack(Vector2 direction)
+    {
+        GameObject newProjectile = Instantiate(projectileSpawned, transform.position, Quaternion.identity);
+        ProjectileScript projectileScript = newProjectile.GetComponent<ProjectileScript>();
+        projectile = ScriptableObject.CreateInstance<Projectile>();
+        projectile.Initialize(1, 2, direction);
+        projectileScript.Projectile = projectile;
+    }
 }
